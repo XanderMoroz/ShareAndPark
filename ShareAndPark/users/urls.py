04f497d5a775a891b1cfc928f23ca9e1f3711a0b
xander <moroz070688@gmail.com>
2023-03-11 23:53:06 +0300

    [critical] delete pullrequest

--- ShareAndPark/users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import SignUp
-
-urlpatterns = [
-    # path('', HomePage.as_view(), name="account"),                  # Главная страница
-    path("signup/", SignUp.as_view(), name="signup"),                # Поиск машино-мест
-]
-
